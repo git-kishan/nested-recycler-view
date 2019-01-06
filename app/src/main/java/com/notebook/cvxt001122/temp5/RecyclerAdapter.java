8edbc32cbde26b02b73fe7854e1cc9cd81e956f0
git-kishan <kishankumar001122@gmail.com>
2019-01-06 14:10:12 +0530

    first commit

--- app/src/main/java/com/notebook/cvxt001122/temp5/RecyclerAdapter.java
+++ app/src/main/java/com/notebook/cvxt001122/temp5/RecyclerAdapter.java
@@ -14,8 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 public class RecyclerAdapter extends RecyclerView.Adapter {
 
     List<Object> data;
-    private int imageModel=1;
-    private int titleModel=2;
+
     LayoutInflater inflater;
     public RecyclerAdapter (List<Object> data){
         this.data=data;
@@ -25,31 +24,14 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         inflater=LayoutInflater.from(parent.getContext());
-        if(viewType==imageModel){
-             View view=inflater.inflate(R.layout.image_layout,parent,false );
-             return new ImageCustomHolder(view);
-        }
-        else  if(viewType==titleModel){
-            View view=inflater.inflate(R.layout.title_layout,parent,false );
-            return new TextViewHolder(view);
-        }
+
         return  null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
-        if(holder instanceof TextViewHolder){
-            TitleModel object= (TitleModel) data.get(position);
-            ((TextViewHolder) holder).title.setText(object.getTitle());
-            ((TextViewHolder) holder).more.setText(object.getMore());
 
-        }
-        else if(holder instanceof  ImageCustomHolder){
-            ImageModel object=(ImageModel)data.get(position);
-            ((ImageCustomHolder) holder).image.setImageResource(object.getImageId());
-            ((ImageCustomHolder) holder).name.setText(object.getName());
-        }
 
     }
 
@@ -62,12 +44,6 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
     public int getItemViewType(int position) {
 
 
-        if(data.get(position) instanceof ImageModel){
-            return imageModel;
-        }
-        else if(data.get(position) instanceof TitleModel){
-            return titleModel;
-        }
         return -1;
 
     }
